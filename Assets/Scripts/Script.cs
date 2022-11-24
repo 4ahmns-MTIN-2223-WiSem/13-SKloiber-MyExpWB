d7e71ce30d0c03426cd9be5a3236531776cb322a
sophxD <Sophie.Kloiber05@htl-Salzburg.ac.at>
2022-11-24 13:12:31 +0100

    Second Upload

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public GameObject zimtstange;
+    public GameObject punsch1;
+
+    public void Start()
+    {
+        
+
+        // Zimtstangen brauchen Physiks 
+        // Glühwein braucht Physiks und einen Wiederstand
+        // und muss a Triggerpunkt werden
+        // Zielscheibe braucht trigger punkte
+
+        //Mein Spiel muss folgende syntaktischen Elemente beinhalten:
+        //Array --> Zimtstangen
+        //If Statements --> Zielscheibe if die Zimtstange trifft a glas
+        //Methoden --> eh überall
+    }
+}
